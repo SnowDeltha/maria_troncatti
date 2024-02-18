40f5759288734cce84483a7aa4d924fac6dc0338
SnowDeltha <deicrolix@gmail.com>
2024-02-18 13:27:26 -0500

    Creacion de ventanasde inicio de sesion

---
+++ lib/presentation/screens/inicio_sesion/inicio_sesion_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class InicioSesionScreen extends StatefulWidget {
+
+  static const name = 'inicio_sesion_screen';
+
+  const InicioSesionScreen({super.key});
+
+  @override
+  State<InicioSesionScreen> createState() => _InicioSesionScreenState();
+}
+
+class _InicioSesionScreenState extends State<InicioSesionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
