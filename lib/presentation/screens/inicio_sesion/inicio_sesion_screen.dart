9aab0864014c639c4fc8358213c3ad2fcfc2fe6c
SnowDeltha <deicrolix@gmail.com>
2024-02-18 21:39:58 -0500

    Se agre el inicio de Sesion

--- lib/presentation/screens/inicio_sesion/inicio_sesion_screen.dart
+++ lib/presentation/screens/inicio_sesion/inicio_sesion_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class InicioSesionScreen extends StatefulWidget {
   static const name = 'inicio_sesion_screen';
@@ -12,6 +13,87 @@ class InicioSesionScreen extends StatefulWidget {
 class _InicioSesionScreenState extends State<InicioSesionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 150,
+        ),
+        children:<Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/Escuela.png'),
+
+              const SizedBox(
+                width: 160,
+                height: 20,
+              ),
+
+              const Text('Gestor de Asistencias',
+              style: TextStyle(
+                fontFamily: 'bold',
+                fontSize: 30,
+                color: Colors.orange
+              ),
+              ),
+
+              const SizedBox(
+                width: 160,
+                height: 20,
+              ),
+
+               TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                
+                decoration: InputDecoration(
+                  //hintText: 'Usuario',
+                  labelText: ' Usuario',
+                  
+                  floatingLabelAlignment: FloatingLabelAlignment.center,
+                  //suffixIcon: Icon(Icons.verified_user),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0)
+                  )
+                ),
+              ),
+
+
+              const SizedBox(
+                width: 160,
+                height: 40,
+              ),
+
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                
+                decoration: InputDecoration(
+                  //hintText: 'Usuario',
+                  labelText: ' Contraseña',
+                  
+                  floatingLabelAlignment: FloatingLabelAlignment.center,
+                  //suffixIcon: Icon(Icons.verified_user),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0)
+                  )
+                ),
+              ),
+
+              const Divider(
+                height: 15,
+              )
+
+              
+
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
