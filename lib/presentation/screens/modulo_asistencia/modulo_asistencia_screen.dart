2f2fb00cf03132260c795125f4df9c10c101dca7
SnowDeltha <deicrolix@gmail.com>
2024-02-19 23:46:23 -0500

    Se agrego el selector de fechas

--- lib/presentation/screens/modulo_asistencia/modulo_asistencia_screen.dart
+++ lib/presentation/screens/modulo_asistencia/modulo_asistencia_screen.dart
@@ -5,17 +5,46 @@ class ModuloAsistenciaScreen extends StatefulWidget {
   const ModuloAsistenciaScreen({super.key});
 
   @override
-  State<ModuloAsistenciaScreen> createState() =>
-      _ModuloAsistenciaScreenState();
+  State<ModuloAsistenciaScreen> createState() => _ModuloAsistenciaScreenState();
 }
 
 class _ModuloAsistenciaScreenState extends State<ModuloAsistenciaScreen> {
+  DateTime selectedDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow,
         title: const Text('Modulo de Asistencia'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 150,
+            ),
+            child: Text(
+              "${selectedDate.year} - ${selectedDate.month} - ${selectedDate.day}",
+            ),
+          ),
+          ElevatedButton(
+            child: const Text("Seleccione Fecha"),
+            onPressed: () async {
+              final DateTime? dateTime = await showDatePicker(
+                context: context,
+                initialDate: selectedDate,
+                firstDate: DateTime(2000),
+                lastDate: DateTime(3000),
+              );
+              if (dateTime != null) {
+                setState(() {
+                  selectedDate = dateTime;
+                });
+              }
+            },
+          ),
+        ],
       ),
     );
   }
