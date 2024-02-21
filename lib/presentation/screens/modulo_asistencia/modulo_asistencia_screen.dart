2cc2dd606025d35d272c90db1ebfadb658f3ed65
SnowDeltha <deicrolix@gmail.com>
2024-02-21 02:30:53 -0500

    Ultimos cambios

--- lib/presentation/screens/modulo_asistencia/modulo_asistencia_screen.dart
+++ lib/presentation/screens/modulo_asistencia/modulo_asistencia_screen.dart
@@ -9,42 +9,12 @@ class ModuloAsistenciaScreen extends StatefulWidget {
 }
 
 class _ModuloAsistenciaScreenState extends State<ModuloAsistenciaScreen> {
-  DateTime selectedDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow,
         title: const Text('Modulo de Asistencia'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 20,
-              horizontal: 150,
-            ),
-            child: Text(
-              "${selectedDate.year} - ${selectedDate.month} - ${selectedDate.day}",
-            ),
-          ),
-          ElevatedButton(
-            child: const Text("Seleccione Fecha"),
-            onPressed: () async {
-              final DateTime? dateTime = await showDatePicker(
-                context: context,
-                initialDate: selectedDate,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(3000),
-              );
-              if (dateTime != null) {
-                setState(() {
-                  selectedDate = dateTime;
-                });
-              }
-            },
-          ),
-        ],
       ),
     );
   }
--- lib/presentation/screens/registro_asistencia/registro_asistencia_screen.dart
+++ lib/presentation/screens/registro_asistencia/registro_asistencia_screen.dart
@@ -10,13 +10,159 @@ class RegistroAsistenciaScreen extends StatefulWidget {
 }
 
 class _RegistroAsistenciaScreenState extends State<RegistroAsistenciaScreen> {
+  DateTime selectedDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.yellow,
-        title: const Text('Registro de Asistecia'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal:70,
+            ),
+            child: Text('Registro de Asistencia',
+            style: TextStyle(
+            ),
+            ),
+          ),
+          
+          backgroundColor: Colors.yellow,
+          ),
+        body: Column(
+          children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 150,
+            ),
+            child: Text(
+              "${selectedDate.year} - ${selectedDate.month} - ${selectedDate.day}",
+            ),
+          ),
+          ElevatedButton(
+            child: const Text("Seleccione Fecha"),
+            onPressed: () async {
+              final DateTime? dateTime = await showDatePicker(
+                context: context,
+                initialDate: selectedDate,
+                firstDate: DateTime(2000),
+                lastDate: DateTime(3000),
+              );
+              if (dateTime != null) {
+                setState(() {
+                  selectedDate = dateTime;
+                });
+              }
+            },
+          ),
+
+          const DataTableExample(),
+
+          const Expanded(
+            child: Divider(
+              thickness: 0.5,
+              color: Colors.white,
+            ),
+          ),
+          // Pie de página
+          Container(
+            color: Colors.green,
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            child: const Text(
+              '©2024 Instituto Tecnológico Superior Japón',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+
+        ],
+        ),
       ),
     );
   }
 }
+
+// Aqui empieza la tabla de datos 
+class DataTableExample extends StatelessWidget {
+  const DataTableExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Name',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'A',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'FJ',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ), 
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'FI',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+      ],
+      rows: const <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Alumno')),
+            DataCell(Text('x')),
+            DataCell(Text('')),
+            DataCell(Text('')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Alumno')),
+            DataCell(Text('')),
+            DataCell(Text('x')),
+            DataCell(Text('')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Alumno')),
+            DataCell(Text('')),
+            DataCell(Text('')),
+            DataCell(Text('x')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Alumno')),
+            DataCell(Text('')),
+            DataCell(Text('x')),
+            DataCell(Text('')),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+  
